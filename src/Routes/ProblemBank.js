3c75820a94eb50bb1ba096039a8d5008488e7bac
Gordon Cheung <74426497+Hgjnnf@users.noreply.github.com>
2022-01-16 11:02:40 -0800

    Update ProblemBank.js

--- src/Routes/ProblemBank.js
+++ src/Routes/ProblemBank.js
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import useState from 'react'
 import useEffect from 'react'
-import Link from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import * as styledComponents from "https://cdn.skypack.dev/styled-components@5.3.3";
 import "../index.css"
 import AiFillLike from "react-icons/ai";
