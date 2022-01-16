dbd4d3a406b77a2b3b74fca0162e5fb32f548851
Gordon Cheung <74426497+Hgjnnf@users.noreply.github.com>
2022-01-16 10:27:51 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import './App.css';
 import ProblemBank from './Routes/ProblemBank';
 import Post from './Routes/Post';
 import React from 'react';
-import BrowserRouter as Router from "react-router-dom";
+import BrowserRouter from "react-router-dom";
 import Routes from "react-router-dom";
 import Route from "react-router-dom";
 import Link from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
     <div className="App">
       <Routes>
         <Route exact path= "/" element = {<ProblemBank />} />
         <Route exact path= "/Post" element = {<Post />} />
       </Routes>
     </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
